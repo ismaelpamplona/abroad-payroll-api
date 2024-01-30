265472eafad64304e37d08459a3dc1aea85d7ebb
Ismael Pamplona <ismaelpamplona@hotmail.com>
2024-01-30 11:23:06 -0300

    (up) Issue #create_roles_crud_endpoints.

--- src/handlers/roles/mod.rs
+++ src/handlers/roles/mod.rs
@@ -2,8 +2,10 @@ use serde::{Deserialize, Serialize};
 use sqlx::FromRow;
 use uuid::Uuid;
 
+pub mod get_by_id;
 pub mod list;
 
+pub use get_by_id::get_by_id;
 pub use list::list;
 
 #[derive(Serialize, FromRow)]
--- src/routes/roles.rs
+++ src/routes/roles.rs
@@ -1,10 +1,8 @@
 use crate::handlers::roles;
-use axum::{
-    routing::{get, post},
-    Extension, Router,
-};
-use sqlx::PgPool;
+use axum::{routing::get, Router};
 
 pub fn routes() -> Router {
-    Router::new().route("/", get(roles::list))
+    Router::new()
+        .route("/", get(roles::list))
+        .route("/:id", get(roles::get_by_id))
 }
