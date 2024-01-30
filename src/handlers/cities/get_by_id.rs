b7ffe9c1ecdc6c07b235d0fee895d9520d033acb
Ismael Pamplona <ismaelpamplona@hotmail.com>
2024-01-30 11:02:48 -0300

    (up) Issue #create_cities_crud_endpoints.

--- src/handlers/cities/get_by_id.rs
+++ src/handlers/cities/get_by_id.rs
@@ -1,42 +1,61 @@
-// use axum::{
-//     extract::{Extension, Path},
-//     http::StatusCode,
-//     response::IntoResponse,
-//     Json,
-// };
-// use sqlx::PgPool;
-// use uuid::Uuid;
+use axum::{
+    extract::{Extension, Path},
+    http::StatusCode,
+    response::IntoResponse,
+    Json,
+};
+use sqlx::PgPool;
+use uuid::Uuid;
 
-// use crate::response::{ApiResponse, ErrorDetail, Meta};
+use crate::response::{ApiResponse, ErrorDetail, Meta};
 
-// use super::*;
+use super::*;
 
-// pub async fn get_by_id(
-//     Extension(pool): Extension<PgPool>,
-//     Path(id): Path<Uuid>,
-// ) -> impl IntoResponse {
-//     let query = sqlx::query_as::<_, BankResponse>("SELECT * FROM banks WHERE id = $1").bind(&id);
+pub async fn get_by_id(
+    Extension(pool): Extension<PgPool>,
+    Path(id): Path<Uuid>,
+) -> impl IntoResponse {
+    let query = format!(
+        "SELECT 
+            cities.id as id,
+            cities.name as name,
+            countries.id as country_id,
+            countries.name as country,
+            cities.latitude,
+            cities.longitude,
+            cities.fc_rb,
+            cities.fc_irex
+        FROM 
+            cities
+        JOIN 
+            countries ON cities.country = countries.id
+        WHERE 
+            cities.id = $1" // Use $1 as a parameter placeholder
+    );
 
-//     let result = query.fetch_all(&pool).await;
+    let result = sqlx::query_as::<_, CityResponse>(&query)
+        .bind(&id)
+        .fetch_all(&pool)
+        .await;
 
-//     match result {
-//         Ok(items) => {
-//             let meta = Meta {
-//                 total_count: Some(1),
-//                 page: Some(1),
-//                 page_size: Some(1),
-//             };
-//             let response = ApiResponse::success_list(items, meta);
-//             (StatusCode::OK, Json(response)).into_response()
-//         }
-//         Err(error) => {
-//             eprintln!("Failed to fetch banks: {}", error);
-//             let error = ErrorDetail {
-//                 code: StatusCode::INTERNAL_SERVER_ERROR.as_u16(),
-//                 message: "Internal server error".to_string(),
-//             };
-//             let response: ApiResponse<String> = ApiResponse::error(error);
-//             (StatusCode::INTERNAL_SERVER_ERROR, Json(response)).into_response()
-//         }
-//     }
-// }
+    match result {
+        Ok(items) => {
+            let meta = Meta {
+                total_count: Some(1),
+                page: Some(1),
+                page_size: Some(1),
+            };
+            let response = ApiResponse::success_list(items, meta);
+            (StatusCode::OK, Json(response)).into_response()
+        }
+        Err(error) => {
+            eprintln!("Failed to fetch cities: {}", error);
+            let error = ErrorDetail {
+                code: StatusCode::INTERNAL_SERVER_ERROR.as_u16(),
+                message: "Internal server error".to_string(),
+            };
+            let response: ApiResponse<String> = ApiResponse::error(error);
+            (StatusCode::INTERNAL_SERVER_ERROR, Json(response)).into_response()
+        }
+    }
+}
--- src/routes/cities.rs
+++ src/routes/cities.rs
@@ -2,5 +2,7 @@ use crate::handlers::cities;
 use axum::{routing::get, Router};
 
 pub fn routes() -> Router {
-    Router::new().route("/", get(cities::list))
+    Router::new()
+        .route("/", get(cities::list))
+        .route("/:id", get(cities::get_by_id))
 }
