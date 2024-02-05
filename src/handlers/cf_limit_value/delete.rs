96428248ca117d5fbfdf2c87bcac4cb3cff32fc1
Ismael Pamplona <ismaelpamplona@hotmail.com>
2024-02-05 16:45:45 -0300

    (up) Issue #cf_limit_exchange_rate_endpoints.

---
+++ src/handlers/cf_limit_value/delete.rs
@@ -0,0 +1,32 @@
+use super::*;
+use crate::response::{ErrorDetail, SuccessDelete};
+use axum::extract::Path;
+
+pub async fn delete(Extension(pool): Extension<PgPool>, Path(id): Path<Uuid>) -> impl IntoResponse {
+    let delete_query = sqlx::query("DELETE FROM cf_limit_value WHERE id = $1")
+        .bind(&id)
+        .execute(&pool);
+
+    match delete_query.await {
+        Ok(delete_result) => {
+            if delete_result.rows_affected() == 0 {
+                let not_found_id = ErrorDetail {
+                    code: StatusCode::NOT_FOUND.as_u16(),
+                    message: format!("cf_limit_value with id {} does not exist.", id),
+                };
+                let res: ApiResponse<String> = ApiResponse::error(not_found_id);
+                return (StatusCode::NOT_FOUND, Json(res)).into_response();
+            }
+
+            let res = ApiResponse::<SuccessDelete>::success_delete(id);
+            (StatusCode::OK, res).into_response()
+        }
+        Err(error) => {
+            eprintln!("Failed to delete cf_limit_value: {}", error);
+            let err = handle_error(&error);
+
+            let res: ApiResponse<String> = ApiResponse::error(err);
+            (get_error_status(&error), Json(res)).into_response()
+        }
+    }
+}
---
+++ src/handlers/cf_limit_value/list.rs
@@ -0,0 +1,55 @@
+use super::*;
+use crate::response::{generate_filter_clauses, Filter, Operator, Pagination};
+use axum::extract::Query;
+
+pub async fn list(
+    Extension(pool): Extension<PgPool>,
+    Query(pagination): Query<Pagination>,
+    Query(filters): Query<CFLimitFilter>,
+) -> impl IntoResponse {
+    let filters = vec![Filter {
+        name: "l.law",
+        op: Operator::ILIKE,
+        val: filters.laws.as_ref(),
+        conj: "OR",
+    }];
+
+    let where_clause = generate_filter_clauses(filters);
+
+    let count_query = format!("SELECT COUNT(*) FROM cf_limit_value l {}", where_clause);
+    let total_count: i64 = sqlx::query_scalar(&count_query)
+        .fetch_one(&pool)
+        .await
+        .unwrap_or(0);
+    let offset = pagination.offset(total_count as usize);
+    let page_size = pagination.page_size(total_count as usize);
+    let query = format!(
+        "{} {} ORDER BY l.law_date DESC LIMIT {} OFFSET {}",
+        SELECT_QUERY, where_clause, page_size, offset
+    );
+
+    let result = sqlx::query_as::<_, CFLimitResponse>(&query)
+        .bind(page_size as i64)
+        .bind(offset as i64)
+        .fetch_all(&pool)
+        .await;
+
+    match result {
+        Ok(items) => {
+            let meta = Meta {
+                total_count: Some(total_count as usize),
+                page: Some(pagination.page.unwrap_or(1)),
+                page_size: Some(page_size),
+            };
+            let response = ApiResponse::success_list(items, meta);
+            (StatusCode::OK, Json(response)).into_response()
+        }
+        Err(error) => {
+            eprintln!("Failed to fetch cf_limit_value: {}", error);
+            let err = handle_error(&error);
+
+            let res: ApiResponse<String> = ApiResponse::error(err);
+            (get_error_status(&error), Json(res)).into_response()
+        }
+    }
+}
