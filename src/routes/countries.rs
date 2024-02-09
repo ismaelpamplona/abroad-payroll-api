2b5e14bc409681463c635b9a39a1d14695beac89
Ismael Pamplona <ismaelpamplona@hotmail.com>
2024-02-09 14:43:22 -0300

    (up) Issue #create_countries_crud_endpoints.

--- src/routes/countries.rs
+++ src/routes/countries.rs
@@ -1,11 +1,18 @@
 use crate::handlers::countries;
+use crate::middlewares::check_etag::check_etag;
+
 use axum::{
+    middleware::from_fn,
     routing::{delete, get, patch, post},
     Router,
 };
 
 pub fn routes() -> Router {
     Router::new()
+        .route(
+            "/:id",
+            patch(countries::update).route_layer(from_fn(check_etag)),
+        )
         .route("/", get(countries::list))
         .route("/:id", get(countries::get_by_id))
         .route("/", post(countries::save))
