b78a3205b5e97fcb8886c7635273670be988c106
Ismael Pamplona <ismaelpamplona@hotmail.com>
2024-02-09 14:40:00 -0300

    (up) Issue #create_cf_limit_exchange_rate_endpoints.

--- src/routes/cf_limit_exchange_rate.rs
+++ src/routes/cf_limit_exchange_rate.rs
@@ -1,11 +1,18 @@
 use crate::handlers::cf_limit_exchange_rate;
+use crate::middlewares::check_etag::check_etag;
+
 use axum::{
+    middleware::from_fn,
     routing::{delete, get, patch, post},
     Router,
 };
 
 pub fn routes() -> Router {
     Router::new()
+        .route(
+            "/:id",
+            patch(cf_limit_exchange_rate::update).route_layer(from_fn(check_etag)),
+        )
         .route("/", get(cf_limit_exchange_rate::list))
         .route("/:id", get(cf_limit_exchange_rate::get_by_id))
         .route("/", post(cf_limit_exchange_rate::save))
