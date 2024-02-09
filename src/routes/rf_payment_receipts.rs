e9d83a1c07f80ccb0ae1f4bcbc49ebc7a6fadbb7
Ismael Pamplona <ismaelpamplona@hotmail.com>
2024-02-09 14:48:59 -0300

    (up) Issue #create_rf_payment_receipts_crud_endpoints.

--- src/routes/rf_payment_receipts.rs
+++ src/routes/rf_payment_receipts.rs
@@ -1,11 +1,18 @@
 use crate::handlers::rf_payment_receipts;
+use crate::middlewares::check_etag::check_etag;
+
 use axum::{
+    middleware::from_fn,
     routing::{delete, get, patch, post},
     Router,
 };
 
 pub fn routes() -> Router {
     Router::new()
+        .route(
+            "/:id",
+            patch(rf_payment_receipts::update).route_layer(from_fn(check_etag)),
+        )
         .route("/", get(rf_payment_receipts::list))
         .route("/:id", get(rf_payment_receipts::get_by_id))
         .route("/", post(rf_payment_receipts::save))
