e1d09ee6a1de3f18beeb0c9879f41f279224e0b4
Ismael Pamplona <ismaelpamplona@hotmail.com>
2024-03-01 18:26:55 -0300

    (up) Issue #create_paid_rf_receipts_endpoints.

--- src/handlers/paid_rf_receipts/mod.rs
+++ src/handlers/paid_rf_receipts/mod.rs
@@ -19,6 +19,36 @@ pub use list::list;
 //     CONSTRAINT paid_rf_receipts_pkey PRIMARY KEY (id)
 // );
 
+// CREATE TABLE public.payroll_closed (
+//     id uuid NOT NULL DEFAULT uuid_generate_v4(),
+//     closed_id uuid NOT NULL,
+//     payroll_item uuid NULL,
+//     person_id uuid NULL,
+//     value float8 NOT NULL,
+//     "date" date NOT NULL,
+// 	created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+//     CONSTRAINT payroll_closed_pkey PRIMARY KEY (id)
+// );
+
+// CREATE TABLE public.people (
+//     id uuid NOT NULL DEFAULT uuid_generate_v4(),
+//     name varchar(300) NOT NULL,
+//     role_id uuid NOT NULL,
+//     class_id uuid NOT NULL,
+//     cpf varchar(11) NOT NULL,
+//     bank_id uuid NOT NULL,
+//     bank_agency varchar(20) NOT NULL,
+//     bank_agency_account varchar(20) NOT NULL,
+//     has_retention_bonus BOOLEAN NOT NULL DEFAULT false,
+//     payroll_brl_pss float8 NOT NULL,
+//     created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+//     updated_at timestamp NULL,
+//     e_tag varchar(100) NOT NULL DEFAULT uuid_generate_v4(),
+//     CONSTRAINT people_cpf_check CHECK (length(cpf) = 11),
+//     CONSTRAINT people_cpf_key UNIQUE (cpf),
+//     CONSTRAINT people_pkey PRIMARY KEY (id)
+// );
+
 #[derive(Serialize, Deserialize, FromRow, Debug)]
 pub struct ManualEntryPayload {
     rf_receipt_id: Uuid,
@@ -52,22 +82,26 @@ pub struct ManualEntryFilter {
 
 pub const SELECT_QUERY: &str = "
     SELECT 
-        m.id,
-        m.person_id,
+        pr.id,
+        pr.rf_receipt_id,
+        pr.payroll_closed_item_id,
+        pc.person_id,
         p.name as person_name,
-        m.payroll_item as payroll_item_id,
-        i.code as payroll_item_code,
-        i.short_name as payroll_item_short_name,
-        i.description as payroll_item_description,
-        m.value,
-        m.start_date,
-        m.end_date,
-        m.e_tag
-    FROM manual_entries m";
+        p.role_id,
+        r.name,
+        p.class_id,
+        c.name,
+        fr.value as fc_by_role,
+        fc.value as fc_by_city,
+    FROM paid_rf_receipts pr";
 
 pub const JOINS_QUERY: &str = "
-    JOIN people p ON p.id = m.person_id
-    JOIN meta_payroll_items i ON i.id = m.payroll_item
+    JOIN payroll_closed pc ON pc.id = pr.payroll_closed_item_id
+    JOIN people p ON p.id = pc.person_id
+    JOIN roles r ON r.id = p.role_id
+    JOIN classes c ON c.id = p.class_id
+    JOIN fc_rf_by_roles fr ON fr.role_id = p.role_id AND fr.class_id = p.class_id
+    JOIN fc_rf_by_city fc ON fc.role_id = p.role_id AND fc.class_id = p.class_id
     ";
 
 pub const RETURN_QUERY: &str = "
